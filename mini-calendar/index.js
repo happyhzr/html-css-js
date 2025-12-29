390be6ed63565081661262c1bfe32ccbc2285b34
happyhzr <insisthzr@163.com>
2025-12-29 20:22:27 +0800

    mini calendar

---
+++ mini-calendar/index.js
@@ -0,0 +1,10 @@
+const monthNameEl = document.getElementById('month-name')
+const dayNameEl = document.getElementById('day-name')
+const dayNumberEl = document.getElementById('day-number')
+const yearEl = document.getElementById('year')
+
+const date = new Date()
+monthNameEl.textContent = date.toLocaleString('en-US', { month: 'long' })
+dayNameEl.textContent = date.toLocaleString('en-US', { weekday: 'long' })
+dayNumberEl.textContent = date.getDate()
+yearEl.textContent = date.getFullYear()
